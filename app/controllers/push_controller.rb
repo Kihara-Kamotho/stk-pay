930a75402c411b28bd6007d5acdbbb4f1aa2fdca
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-01-16 19:09:26 +0300

    added mpesa module into services, push controller and routes

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -1,6 +1,8 @@
 class PushController < ApplicationController 
-
-  def index 
+  skip_before_action :verify_authenticity_token
+  
+  def index
+    @payments = Payment.all 
   end 
 
   def payment 
